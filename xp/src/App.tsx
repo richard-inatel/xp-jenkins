0b16ad6be6e6c36cdf6f18eb000e2045d5b86108
Richard Matheus <richard@inatel.br>
2020-08-20 13:52:41 -0300

    Updating react app v2

--- xp/src/App.tsx
+++ xp/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to XP about Jenkins
+          Welcome to XP about Jenkins v2
         </p>
       </header>
     </div>
