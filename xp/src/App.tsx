fc2c68bdb91a2120cb6559b1ea3f07a8162f31e8
Richard Matheus <richard@inatel.br>
2020-08-20 16:50:41 -0300

    React app v3

--- xp/src/App.tsx
+++ xp/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to XP about Jenkins v2
+          Welcome to XP about Jenkins v3 16:50h
         </p>
       </header>
     </div>
